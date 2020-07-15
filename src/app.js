657c5be4525d81787ec3b895172458c664a82379
WelkerTsz <welkertsz@gmail.com>
2020-07-15 20:55:23 -0300

    Trabalhando com middlewares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 const app = require('express')();
+const consign = require('consign');
+
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
+
 
 app.get('/', (req, res) => res.status(200).send());
 
@@ -7,6 +13,10 @@ app.get('/users', (req, res) => {
     { name: 'Teste', mail: 'teste@mail.com' },
   ];
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 
 const app = require('../src/app');
-test('Respondendo na raiz do app', () => request(app).get('/')
+test('Verificando a raiz', () => request(app).get('/')
   .then((res) => {
     expect(res.status).toBe(200);
   }));
